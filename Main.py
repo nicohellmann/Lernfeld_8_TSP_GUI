63e39d00b20cb747953e45aa45b9a29b8762c3ef
Nico Hellmann <hellmann1.nico@gmx.de>
2024-02-15 19:17:32 +0100

    added method to point class to calculate distanz, added commentary for better readability, began tsp function

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from Point import Point
 import random
-route = []
+
 def quit(window):
     window.quit()
-
+####### --- Functions for mouse and Buttons --- #######
 def draw_point(event):
     global x,y
     global point_counter
@@ -13,20 +13,16 @@ def draw_point(event):
     y = (event.y)
     can.create_oval(x,y,x,y,width=5,fill="black")
     can.create_text(x+7,y,text=letters[point_counter])
+    route.append(Point(x,y))
     point_counter +=1
     if point_counter >=26:
         point_counter =0
+    print(len(route))
 
-def click_corrdinates(event):
-    global x,y
-    x = (event.x)
-    y = (event.y)
-    print(str(x) +'/' +str(y))
 def draw_cities():
     global point_counter
     
     letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    print(dot_input.get())
     city_count = int(str(dot_input.get()))
     for i in range(0,city_count):
         x = random.randint(0,500)
@@ -37,6 +33,7 @@ def draw_cities():
         point_counter +=1
         if point_counter >=26:
             point_counter =0
+    print(len(route))
 
 
 def draw_line(event):
@@ -51,21 +48,33 @@ def draw_line(event):
         y2 = event.y
         can.create_line(x1,y1,x2,y2,fill="blue")
         click_number=0
-window = Tk()
-dot_input = Entry(window)
-exit_button= Button(window,text="Exit",background="red",command=lambda:quit(window))
-tsp_button = Button(window,text="Cities", background="green",command=lambda:draw_cities())
-can = Canvas(window,width=500, height=500)
-exit_button.grid(row=0,column=0)
-tsp_button.grid()
-dot_input.grid()
-can.bind("<Button-1>",click_corrdinates)
-can.bind("<Button-1>",draw_point)
 
+def tsp():
+    for i in range(len(route)-1):
+        can.create_line(route[i].x,route[i].y,route[i+1].x,route[i+1].y,fill="blue")
+    can.create_line(route[len(route)-1].x,route[len(route)-1].y,route[0].x,route[0].y,fill="blue")
+####### --- window --- #######
+window = Tk()
+####### --- Input --- #######
+dot_input = Entry(window)
+####### --- Buttons --- #######
+exit_button= Button(window,text="Exit",background="red",command=lambda:quit(window))
+generate_cities_button = Button(window,text="add Cities", background="green",command=lambda:draw_cities())
+tsp_button=Button(window,text="TSP",background="blue", command=lambda:tsp())
+####### --- canvas --- #######
+can = Canvas(window,width=500, height=500)
+####### --- placing of Entry widgets, Labels, Buttons --- #######
+exit_button.grid(row=0,column=0)
+generate_cities_button.grid(row=1,column=0)
+dot_input.grid()
+tsp_button.grid()
+can.grid()
+####### --- binding functions and mouse input --- #######
+can.bind("<Button-1>",draw_point)
 can.bind("<Button-3>",draw_line)
+####### --- variables --- #######
 point_counter=0
 click_number=0
-
-
-can.grid()
+route = []
+####### --- mainloop --- #######
 mainloop()
--- Point.py
+++ Point.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
 class Point:
 
     def __init__(self,x:int,y:int) -> None:
         self.x = x
         self.y = y
+
+    def entfernung(self,point2:Point):
+        xentf = self.x - point2.x
+        yentf = self.y - point2.y
+        return (xentf**2 + yentf**2)**0.5
